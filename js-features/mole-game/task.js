eb6fe79321628253a79d78468f3021bcc7055fe1
Mikhail Kucherenko <mihail.k.work@mail.ru>
2023-01-15 16:52:32 +0300

    js-features-3

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,17 +1,30 @@
-const hole = document.getElementsByClassName("hole");
-let arr = [];
-for (let i = 0; i < hole.length; i++) {
-    arr.push(hole[i].id);
-}
-// alert(arr);
+const holes = document.getElementsByClassName("hole");
+// console.log(holes[1].id);
+// alert(holes[1].id);
+// console.log(holes[1].className);
 
-for (let i = 0; i < arr.length; i++)
-hole.onclick = function() {
-    if (hole.className.includes( 'hole_has-mole' )) {
-        const clickerTwo = document.getElementById("dead");
-        clickerTwo.textContent = Number(clickerTwo.textContent) + 1;
-    } else {
-        const clicker = document.getElementById("lost");
-        clicker.textContent = Number(clicker.textContent) + 1;
-    }
+for (let i = 0; i < holes.length; i++) {
+    holes[i].onclick = function() {
+        if (holes[i].className.includes( 'hole_has-mole' )) {
+            const clickerDead = document.getElementById("dead");
+                if (clickerDead.textContent >= 9) {
+                    alert('Победа');
+                } clickerDead.textContent = Number(clickerDead.textContent) + 1;
+        } else {
+            const clickerLost = document.getElementById("lost");
+                if (clickerLost.textContent >= 4) {
+                    alert('Вы проиграли!');
+                } clickerLost.textContent = Number(clickerLost.textContent) + 1;
+        }
+    }     
 }
+
+// for (let i = 0; i < holes.length; i++) {
+//     holes[i].onclick = function() {
+//         if (holes[i].className.includes( 'hole_has-mole' )) {
+//             alert('Hi'); 
+//         } else {
+//             alert('Bye');
+//         }
+//     }     
+// }
