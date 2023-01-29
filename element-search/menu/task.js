ece1787fdbe04181d99306e11be584c00095a780
Mikhail Kucherenko <mihail.k.work@mail.ru>
2023-01-29 14:12:01 +0300

    element-search-3

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,49 +1,18 @@
-const menuLink = Array.from(document.querySelectorAll('.menu__link'));
-    for (let i = 0; i < menuLink.length; i++) {
-        // console.log(menuLink);
-
-        menuLink[i].onclick = function() {
-            const menuItem = menuLink[i].closest('.menu__item');
-            for (let i = 0; i < menuItem.length; i++) {
-                const menuSub = menuItem[i].closest('.menu_sub');
-                for (let i = 0; i < menuSub.length; i++) {
-                    menuSub.classList.toggle('menu_active');
-                    return false;
-                }
-
-            }
+const menuSub = document.querySelectorAll('.menu_sub');
+  for (let i = 0; i < menuSub.length; i++) {
+    const menuItem = menuSub[i].closest('.menu__item');
+    menuItem.onclick = function() {
+      for (let i = 0; i < menuSub.length; i++) {
+        if (menuSub[i].className.includes('menu_active')) {
+          menuSub[i].className.remove('menu_active');
         }
-
-
-
-        // menuLink[i].onclick = function() {
-        //     const menuItem = menuLink[i].closest('.menu__item');
-        //     for (let i = 0; i < menuItem.length; i++) {
-        //         if (menuItem[i].parentElement.className.includes( 'menu_sub' )) {
-        //             menuItem[i].parentElement.classList.toggle('menu_active');
-        //             return false;
-        //         }
-        //     }
-        // }
-
-        // menuLink[i].onclick = function() {
-        //     const menu = menuLink[i].closest('.menu');
-        //         if (menu.className.includes( 'menu_sub' )) {
-        //             menu.classList.toggle('menu_active');
-        //             return false;
-        //         }
-        // }
-
-        // menuLink[i].onclick = function() {
-        //     const menu = menuLink[i].closest('.menu_sub');
-        //     menu.classList.toggle('menu_active');
-        //     return false;
-        // }
-
-        // menuLink[i].onclick = function() {
-        //     const menu = menuLink[i].closest('.menu');
-        //     menu.classList.toggle('menu_active');
-        //     return false;
-        // }
-
     }
+    menuSub[i].classList.toggle('menu_active');
+    return false;
+    //alert('Hi');
+    }
+  }
+
+// const menuLink = Array.from(document.querySelectorAll('.menu__link'));
+//     for (let i = 0; i < menuLink.length; i++) {
+//         // console.log(menuLink);
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -4,34 +4,32 @@ const prevButton = document.getElementsByClassName('slider__arrow_prev');
 // const nextButton = {...document.getElementsByClassName('slider__arrow_next')};
 // const prevButton = {...document.getElementsByClassName('slider__arrow_prev')}; 
 
-for (let i = 0; i < nextButton.length; i++) {
-	nextButton[i].onclick = next;
-	prevButton[i].onclick = prev;
-}
-
-// nextButton.onclick = next;
-// prevButton.onclick = prev;
+// for (let i = 0; i < nextButton.length; i++) {
+nextButton[0].onclick = next;
+prevButton[0].onclick = prev;
+// }
 
 function getCurrentIndex() {
-    for (let i = 0; i < sliderItems.length; i++)
-    if (sliderItems[i].className.includes( 'slider__item_active' )) {
-        return i; 
-    }
- }
+    // for (let i = 0; i < sliderItems.length; i++)
+    // if (sliderItems[i].className.includes( 'slider__item_active' )) {
+    //     return i; 
+    // }
+  return sliderItems.findIndex(slider => slider.className.includes( 'slider__item_active' ));
+}
 
 function next() {
-	const current = getCurrentIndex();
-	const active = getCurrentIndex() === sliderItems.length - 1 ? 0 : getCurrentIndex() + 1;
-	changeSlide(current, active);
+  const current = getCurrentIndex();
+  const active = current === sliderItems.length - 1 ? 0 : current + 1;
+  changeSlide(current, active);
  }
 
 function prev() {
-	const current = getCurrentIndex();
-	const active = getCurrentIndex() === 0 ? sliderItems.length - 1 : getCurrentIndex() - 1;
-	changeSlide(current, active);
+  const current = getCurrentIndex();
+  const active = current === 0 ? sliderItems.length - 1 : current - 1;
+  changeSlide(current, active);
  }
 
 function changeSlide(current, active) {
-	sliderItems[current].classList.remove('slider__item_active');
-	sliderItems[active].classList.add('slider__item_active');
+  sliderItems[current].classList.remove('slider__item_active');
+  sliderItems[active].classList.add('slider__item_active');
 }
