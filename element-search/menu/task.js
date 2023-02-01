8591f182836f9d9637ccfc0fd4cf9046d4966d7a
Mikhail Kucherenko <mihail.k.work@mail.ru>
2023-02-01 21:28:19 +0300

    element-search-5

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,15 +1,19 @@
-const menuSub = document.querySelectorAll('.menu_sub');
-  for (let i = 0; i < menuSub.length; i++) {
-    const menuItem = menuSub[i].closest('.menu__item');
-    menuItem.onclick = function() {
-      for (let i = 0; i < menuSub.length; i++) {
-        if (menuSub[i].className.includes('menu_active')) {
-        menuSub[i].classList.remove('menu_active');
-        return;
-        }
-      }           
-      menuSub[i].classList.toggle('menu_active');
-      return false;
-    }
-  }
+const menuSub = document.querySelectorAll('.menu_sub');              
+	for (let i = 0; i < menuSub.length; i++) {                         
+		const menuItem = menuSub[i].closest('.menu__item');              
+			menuItem.onclick = function() {                              
+				for (let i = 0; i < menuSub.length; i++) {               
+					if (menuSub[i].className.includes('menu_active')) {
+						menuSub[i].classList.remove('menu_active');
+						if (menuSub[i] != this.menuSub) {
+						this.menuSub.classList.add('menu_active');
+            return false; 
+						} 
+						return;
+					} 
+				}  
+				menuSub[i].classList.toggle('menu_active');
+        return false;
+		  }
+}  
 
