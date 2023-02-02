89116e49747e27d979f4980d2de04ba739971c92
Mikhail Kucherenko <mihail.k.work@mail.ru>
2023-02-02 22:19:38 +0300

    element-search-6

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,19 +1,22 @@
 const menuSub = document.querySelectorAll('.menu_sub');              
-	for (let i = 0; i < menuSub.length; i++) {                         
-		const menuItem = menuSub[i].closest('.menu__item');              
-			menuItem.onclick = function() {                              
-				for (let i = 0; i < menuSub.length; i++) {               
-					if (menuSub[i].className.includes('menu_active')) {
-						menuSub[i].classList.remove('menu_active');
-						if (menuSub[i] != this.menuSub) {
-						this.menuSub.classList.add('menu_active');
-            return false; 
-						} 
-						return;
-					} 
-				}  
-				menuSub[i].classList.toggle('menu_active');
-        return false;
-		  }
-}  
-
+for (let i = 0; i < menuSub.length; i++) {                         
+  const menuItem = menuSub[i].closest('.menu__item');              
+    menuItem.onclick = function() {
+      const activeSubMenu = document.querySelector('.menu_active');
+	  // alert(activeSubMenu);                              
+      for (let i = 0; i < menuSub.length; i++) {
+        if (activeSubMenu != null) {
+        // alert('В 1м if');
+          activeSubMenu.classList.remove('menu_active');	
+        };
+        if (activeSubMenu === menuSub[i]) {
+        // alert('Во 2м if');
+          menuSub[i].classList.add('menu_active');
+          return;
+        }	 
+      } 
+	// alert('В toggle');
+	menuSub[i].classList.toggle('menu_active');
+	return false;
+    }
+}
