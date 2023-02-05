cb99e389914624d0b6ea16033a2a1a70ba74345c
Mikhail Kucherenko <mihail.k.work@mail.ru>
2023-02-05 11:43:58 +0300

    element-search-7

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,20 +2,14 @@ const menuSub = document.querySelectorAll('.menu_sub');
 for (let i = 0; i < menuSub.length; i++) {                         
   const menuItem = menuSub[i].closest('.menu__item');              
     menuItem.onclick = function() {
-      const activeSubMenu = document.querySelector('.menu_active');
-	  // alert(activeSubMenu);                              
+      const activeSubMenu = document.querySelector('.menu_active');                             
       for (let i = 0; i < menuSub.length; i++) {
-        if (activeSubMenu != null) {
-        // alert('В 1м if');
-          activeSubMenu.classList.remove('menu_active');	
-        };
-        if (activeSubMenu === menuSub[i]) {
-        // alert('Во 2м if');
-          menuSub[i].classList.add('menu_active');
-          return;
-        }	 
+        if (activeSubMenu != null && activeSubMenu !== menuSub[i]) { // открытое подменю существует и не является тем, по которому был клик
+          activeSubMenu.classList.remove('menu_active'); // закрываем предыдущее подменю
+          menuSub[i].classList.add('menu_active'); // открываем новое подменю
+          return false;
+        }
       } 
-	// alert('В toggle');
 	menuSub[i].classList.toggle('menu_active');
 	return false;
     }
