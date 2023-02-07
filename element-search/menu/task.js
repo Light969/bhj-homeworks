1950531373a4a95d13e5140be26e9dd9490a597c
Mikhail Kucherenko <mihail.k.work@mail.ru>
2023-02-07 20:37:03 +0300

    element-search-9

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,24 +1,39 @@
 const menuSub = document.querySelectorAll('.menu_sub');              
 for (let i = 0; i < menuSub.length; i++) {                         
-  const menuItem = menuSub[i].closest('.menu__item');              
-  menuItem.onclick = function() {
-    // console.log(menuItem.closest('.menu_sub'));
-    // alert(menuItem.closest('.menu_sub'));
-    if (menuItem.closest('.menu_sub') != null) {
-      return false;
-    } else {
-    const activeSubMenu = document.querySelector('.menu_active'); 
-    // if (activeSubMenu != null && activeSubMenu != menuSub[i] && menuItem.closest('.menu_sub') === null)                            
-    if (activeSubMenu != null && activeSubMenu != menuSub[i]) { // открытое подменю существует и не является тем, по которому был клик
-      activeSubMenu.classList.remove('menu_active'); // закрываем предыдущее подменю
-      menuSub[i].classList.add('menu_active'); // открываем новое подменю
-      return false;
-    }
-  menuSub[i].classList.toggle('menu_active');
-  return false;
-    }
+  const menuItem = menuSub[i].closest('.menu__item'); 
+  const menuLink = menuItem.querySelector('a');
+  menuLink.onclick = function() {
+    const activeSubMenu = document.querySelector('.menu_active'); 	   
+	if (activeSubMenu != null && activeSubMenu != menuSub[i]) { 
+	activeSubMenu.classList.remove('menu_active');
+	} 	  
+    menuSub[i].classList.toggle('menu_active');
+    return false;
   }
 }
+
+
+//-------------------Второй рабочий вариант решения
+
+// const menuLink = [...document.querySelectorAll('.menu__link')];
+// menuLink.forEach((element) => {
+//   element.onclick = function () {
+//     const menuItem = element.closest('.menu__item');
+// 	const menuSub = menuItem.querySelector('.menu_sub');
+//     const activeSubMenu = document.querySelector('.menu_active');
+// 	  if (menuSub != activeSubMenu && activeSubMenu !=null) {
+//         activeSubMenu.classList.remove('menu_active');		
+// 		menuSub.classList.add('menu_active');
+//         return false;
+//       }
+//       menuSub.classList.toggle('menu_active');
+//       return false; 
+//   }
+// });
+
+
+
+
 
 
 
