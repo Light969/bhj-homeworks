7462a5eec633c233c4d7343763647cb99a75acc1
Mikhail Kucherenko <mihail.k.work@mail.ru>
2023-03-07 20:51:14 +0300

    Async-Request-2

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,17 +1,31 @@
 document.forms.form.addEventListener('submit', (e) => {
 
     e.preventDefault();
-
     const xhr = new XMLHttpRequest();
-    xhr.addEventListener('readystatechange', () => {
-        if (xhr.readyState === xhr.DONE) {
-            console.log(xhr.responseText);
-        }
-    });
+
+    // отслеживаем процесс отправки
+    xhr.upload.onprogress = function(event) {
+        const progress = document.getElementById( 'progress' );
+        let progressValue = `${event.loaded}` / `${event.total}`;
+        progress.value = progressValue;
+        // console.log(progressValue);
+        console.log(`Отправлено ${event.loaded} из ${event.total}`);
+    };
+
+    // Ждём завершения: неважно, успешного или нет
+    xhr.onloadend = function() {
+      if (xhr.status == 200) {
+        console.log("Успех");
+      } else {
+        console.log("Ошибка " + this.status);
+      }
+    };
     
     xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
     
-    const formData = new FormData(document.forms.form);
+    const formElement = document.getElementById('form');
+    // console.log(formElement);
+    const formData = new FormData(formElement);
     
     xhr.send(formData);
 
