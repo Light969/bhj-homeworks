524fec16346d36583fd6a403f713cbaf05a4394b
Mikhail Kucherenko <mihail.k.work@mail.ru>
2023-04-02 17:25:02 +0300

    Async-Request-3

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -7,21 +7,27 @@ const xhr = new XMLHttpRequest();
 xhr.addEventListener('readystatechange', () => {
     if (xhr.readyState === xhr.DONE) {
         console.log(xhr.responseText);
-        pollTitle.textContent = 'Как вы относитесь к собакам?';
-        pollAnswers.insertAdjacentHTML('afterBegin', '<button class="poll__answer">Хорошо</button><button class="poll__answer">Отлично</button><button class="poll__answer">Я люблю собак</button><button class="poll__answer">Кто тут?</button>');
+        //Проверяем возвращаемые данные
+        // alert(typeof(xhr.responseText));
+        //Преобразуем строку в объект (responseText и response - одно и то же)
+        const object = JSON.parse(xhr.response);
+        // console.log(object);
+        // console.log(object.data.title);
+        // console.log(object.data.answers);
+
+        pollTitle.textContent = `${object.data.title}`;
+        let answers = object.data.answers;
+        // console.log(answers);
+        for (let i = 0; i < answers.length; i++) {
+            pollAnswers.insertAdjacentHTML('afterBegin', `
+            <button class="poll__answer">${answers[i]}</button>
+            `);
+        }
         const pollAnswer = Array.from(document.getElementsByClassName('poll__answer'));
         // console.log(pollAnswer);
         pollAnswer.forEach(answer => answer.onclick = function() {
             alert('Спасибо, ваш голос засчитан!');
         });
-
-        // Тоже самое.
-        // for (let i = 0; i < pollAnswer.length; i++) {
-        //     pollAnswer[i].onclick = function() {
-        //         alert('Спасибо, ваш голос засчитан!');
-        //     }
-        // }
-
     }
 });
 
--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,5 +1,6 @@
 const loader = document.querySelector('.loader');
-const item = Array.from(document.getElementsByClassName('item'));
+// const item = Array.from(document.getElementsByClassName('item'));
+const item = document.querySelector('.item');
 // console.log(item);
 
 
@@ -8,8 +9,29 @@ xhr.addEventListener('readystatechange', () => {
     if (xhr.readyState === xhr.DONE) {
         loader.classList.remove('loader_active');
         // console.log(xhr.responseText);
-        console.log(xhr.CharCode);
-        item[0].insertAdjacentHTML('afterBegin', '<div class="item__code">USD</div><div class="item__value">32</div><div class="item__currency">руб.</div>');
+        // alert(xhr.responseText);
+        //Возвращаемые данные в формате - строки
+        // alert(typeof(xhr.responseText));
+        //Преобразовываем данные в объект
+        function restoreObject() {
+            return JSON.parse(xhr.responseText);
+        }
+        // console.log(restoreObject());
+        const object = restoreObject();
+        // console.log(object);
+        const valute = object.response.Valute;
+        // console.log(valute);
+        for (var key in valute) {
+        //   console.log("Ключ: " + key + " значение: " + valute[key]);
+        //   console.log("Ключ: " + key + " значение: " + valute[key].CharCode);
+        //   console.log("Ключ: " + key + " значение: " + valute[key].Value);
+          item.insertAdjacentHTML('afterBegin', `
+            <div class="item__code">${valute[key].CharCode}</div>
+            <div class="item__value">${valute[key].Value}</div>
+            <div class="item__currency">руб.</div>
+          `);
+        };
+        // item[0].insertAdjacentHTML('afterBegin', '<div class="item__code">USD</div><div class="item__value">32</div><div class="item__currency">руб.</div>');
     }
 });
 
